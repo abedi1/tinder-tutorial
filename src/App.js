9363881e1c79d844b55b71969669eb47304cc37d
AmanB <asbedi@outlook.com>
2022-10-01 16:30:05 -0400

    Created basic template. Can navigate to pages

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import Home from "./pages/Home";
-import Onboarding from "./pages/Onboarding";
+import OnBoarding from "./pages/OnBoarding";
 import Dashboard from "./pages/Dashboard";
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 
 
 const App = () => {
-  return (
-      <BrowserRouter>
-        <Routes>
-          <Route path/>
-        </Routes>
-      </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path={"/"} element={<Home/>}/>
+                <Route path={"/dashboard"} element={<Dashboard/>}/>
+                <Route path={"/onboarding"} element={<OnBoarding/>}/>
+            </Routes>
+        </BrowserRouter>
+    );
 }
 export default App;
 
