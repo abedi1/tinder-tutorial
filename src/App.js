c9f4025f229fef89a211ea5cd1fd669b51b1ddb0
AmanB <asbedi@outlook.com>
2022-10-01 16:25:32 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import Home from "./pages/Home";
+import Onboarding from "./pages/Onboarding";
+import Dashboard from "./pages/Dashboard";
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+
+
+const App = () => {
+  return (
+      <BrowserRouter>
+        <Routes>
+          <Route path/>
+        </Routes>
+      </BrowserRouter>
+  );
+}
+export default App;
+
+//pages necessary
